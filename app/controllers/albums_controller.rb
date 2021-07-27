9f99eeb2dfddfaa547c6565a0d5add4f4ca1358b
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-27 13:03:08 -0700

    Add ability to create new album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,5 +5,13 @@ class AlbumsController < ApplicationController
         render json: @albums
     end
 
+    def create
+        @album = Album.create(album_params)
+        render json: @album
+    end
+
+    def album_params
+        params.permit(:title, :musical_artist, :artist_id)
+    end
     
 end
