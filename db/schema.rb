7a36ea08e714e2c8189216414415e55f812e099b
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-28 07:37:35 -0700

    Fix db table to include only artists and albums

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_203515) do
     t.bigint "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_albums_on_artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -29,158 +28,12 @@ ActiveRecord::Schema.define(version: 2021_02_23_203515) do
     t.datetime "updated_at", null: false
   end
 
-  # create_table "customers", force: :cascade do |t|
-  #   t.string "first_name"
-  #   t.string "last_name"
-  #   t.string "company"
-  #   t.string "address"
-  #   t.string "city"
-  #   t.string "state"
-  #   t.string "country"
-  #   t.string "postal_code"
-  #   t.string "phone"
-  #   t.string "fax"
-  #   t.string "email"
-  #   t.bigint "support_rep_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["support_rep_id"], name: "index_customers_on_support_rep_id"
-  # end
 
-  # create_table "employees", force: :cascade do |t|
-  #   t.string "last_name"
-  #   t.string "first_name"
-  #   t.string "email"
-  #   t.string "title"
-  #   t.integer "reports_to"
-  #   t.date "birth_date"
-  #   t.datetime "hire_date"
-  #   t.string "address"
-  #   t.string "city"
-  #   t.string "state"
-  #   t.string "country"
-  #   t.string "postal_code"
-  #   t.string "phone"
-  #   t.string "fax"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
+  
 
-  # create_table "friendships", force: :cascade do |t|
-  #   t.integer "follower_id"
-  #   t.integer "followee_id"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
 
-  # create_table "genres", force: :cascade do |t|
-  #   t.string "name"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
+  # add_foreign_key "albums", "artists"
 
-  # create_table "invoice_lines", force: :cascade do |t|
-  #   t.bigint "invoice_id"
-  #   t.bigint "track_id"
-  #   t.integer "unit_price"
-  #   t.integer "quantity"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["invoice_id"], name: "index_invoice_lines_on_invoice_id"
-  #   t.index ["track_id"], name: "index_invoice_lines_on_track_id"
-  # end
+  # add_foreign_key "tracks", "albums"
 
-  # create_table "invoices", force: :cascade do |t|
-  #   t.bigint "customer_id"
-  #   t.datetime "invoice_date"
-  #   t.string "billing_address"
-  #   t.string "billing_city"
-  #   t.string "billing_state"
-  #   t.string "billing_country"
-  #   t.string "billing_postal_code"
-  #   t.integer "total"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["customer_id"], name: "index_invoices_on_customer_id"
-  # end
-
-  # create_table "media_types", force: :cascade do |t|
-  #   t.string "name"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-  # create_table "movies", force: :cascade do |t|
-  #   t.string "title"
-  #   t.integer "runtime"
-  #   t.string "image"
-  #   t.string "genre"
-  #   t.string "tagline"
-  #   t.integer "release_year"
-  #   t.text "overview"
-  #   t.integer "rating"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
-
-  # create_table "playlist_tracks", force: :cascade do |t|
-  #   t.bigint "playlist_id"
-  #   t.bigint "track_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   # t.index ["playlist_id"], name: "index_playlist_tracks_on_playlist_id"
-  #   # t.index ["track_id"], name: "index_playlist_tracks_on_track_id"
-  # end
-
-  # create_table "playlists", force: :cascade do |t|
-  #   t.string "name"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-  # create_table "reviews", force: :cascade do |t|
-  #   t.integer "user_id"
-  #   t.integer "movie_id"
-  #   t.text "content"
-  #   t.integer "personal_rating"
-  #   t.integer "likes"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
-
-  create_table "tracks", force: :cascade do |t|
-    t.string "name"
-    t.bigint "album_id"
-    t.bigint "media_type_id"
-    t.bigint "genre_id"
-    t.string "composer"
-    t.integer "milliseconds"
-    t.integer "bytes"
-    t.integer "unit_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["album_id"], name: "index_tracks_on_album_id"
-    t.index ["genre_id"], name: "index_tracks_on_genre_id"
-    t.index ["media_type_id"], name: "index_tracks_on_media_type_id"
-  end
-
-  # create_table "users", force: :cascade do |t|
-  #   t.string "username"
-  #   t.string "password_digest"
-  #   t.string "avatar"
-  #   t.string "email"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
-
-  add_foreign_key "albums", "artists"
-  # add_foreign_key "customers", "employees", column: "support_rep_id"
-  # add_foreign_key "invoice_lines", "invoices"
-  # add_foreign_key "invoice_lines", "tracks"
-  # add_foreign_key "invoices", "customers"
-  # add_foreign_key "playlist_tracks", "playlists"
-  # add_foreign_key "playlist_tracks", "tracks"
-  add_foreign_key "tracks", "albums"
-  # add_foreign_key "tracks", "genres"
-  # add_foreign_key "tracks", "media_types"
 end
